dbfb678c569ea5c19636dced2acf481ab04c63f6
Mansij Maharjan <mansijmaharzn@gmail.com>
2023-01-05 14:31:22 +0545

    fix 1: by bahadur bhai

--- Lab 1/1.cpp
+++ Lab 1/1.cpp
@@ -1,41 +1,48 @@
-// WAP to find the root of a quadratic equation
+/*
+WAP to find the root of a quadratic equation
+
+This one is by Bahadur bhai
+He names functions like passwords ;-;
+*/
 #include<iostream>
-#include<cmath>
+#include<math.h>
 using namespace std;
 
+int square_root(float a, float b, float c) {
+    float sqp1;
+
+    sqp1 = sqrt((b*b) - (4*a*c));
+
+    return sqp1;
+}
+
+int plus1(float sqp1, float a, float b) {
+    float root1;
+
+    root1 = (b+sqp1)/2*a;
+
+    return root1;
+}
+
+int minus1(float sqp1, float a, float b) {
+    float root2;
+
+    root2 = (b-sqp1)/2*a;
+
+    return root2;
+}
+
 int main() {
-    float a, b, c, x1, x2, discriminant, realPart, imaginaryPart;
-    
-    cout << "Enter coefficients a, b and c: ";
+    float sqp1, root1, root2, a, b, c;
+
+    cout << "Enter value of a, b, and c: ";
     cin >> a >> b >> c;
 
-    discriminant = b*b - 4*a*c;
-    
-    if (discriminant > 0) {
-        x1 = (-b + sqrt(discriminant)) / (2*a);
-        x2 = (-b - sqrt(discriminant)) / (2*a);
-        cout << "Roots are real and different." << endl;
-        cout << "x1 = " << x1 << endl;
-        cout << "x2 = " << x2 << endl;
-    } else if (discriminant == 0) {
-        cout << "Roots are real and same." << endl;
-        x1 = -b/(2*a);
-        cout << "x1 = x2 =" << x1 << endl;
-    } else {
-        realPart = -b/(2*a);
-        imaginaryPart =sqrt(-discriminant)/(2*a);
-        cout << "Roots are complex and different."  << endl;
-        cout << "x1 = " << realPart << "+" << imaginaryPart << "i" << endl;
-        cout << "x2 = " << realPart << "-" << imaginaryPart << "i" << endl;
-    }
+    sqp1 = square_root(a, b, c);
+    root1 = plus1(sqp1, a, b);
+    root2 = minus1(sqp1, a, b);
+
+    cout << "Two roots are: " << root1 << " and " << root2;
 
     return 0;
 }
-
-/*
-OUTPUT:
-Enter coefficients a, b and c: 4 5 1
-Roots are real and different.
-x1 = -0.25
-x2 = -1
-*/
