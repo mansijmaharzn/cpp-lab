ee65550ee042b421d60a9490262b7df53ce8b96f
Ankita Maharjan <maharjanankita655@gmail.com>
2023-01-12 08:13:30 +0545

    solved

--- Lab 2/2.cpp
+++ Lab 2/2.cpp
@@ -4,3 +4,28 @@ a. 6km
 b. 8km 100m
 c. 1km 20m 60cm
 */
+
+#include<iostream>
+using namespace std;
+
+double convert(double kilometer, double meter, double centimeter) {
+    meter += (kilometer * 1000);
+    meter += (centimeter/100);
+
+    return meter;
+}
+
+int main() {
+    double kilometer, meter, centimeter, result;
+
+    cout << "Enter 0 if there is no value." << endl;
+    cout << "Enter the values for km m cm" << endl;
+
+    cin >> kilometer >> meter >> centimeter;
+
+    result = convert(kilometer, meter, centimeter);
+
+    cout << "In meter: " << result;
+
+    return 0;
+}
