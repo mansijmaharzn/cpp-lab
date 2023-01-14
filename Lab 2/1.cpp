71f4d43a7e43b2c34573e271ef174692a78d2bb2
Mansij Maharjan <mansijmaharzn@gmail.com>
2023-01-14 21:04:38 +0545

    solved but not with function overloading

--- Lab 2/1.cpp
+++ Lab 2/1.cpp
@@ -6,8 +6,26 @@ c. 4hrs
 */
 
 #include<iostream>
+using namespace std;
+
+double convert(double hours, double minutes, double seconds) {
+    minutes += (hours * 60);
+    minutes += (seconds/60);
+
+    return minutes;
+}
 
 int main() {
+    double hours, minutes, seconds, result;
+
+    cout << "Enter 0 if there is no value." << endl;
+    cout << "Enter the values for hr min sec" << endl;
+
+    cin >> hours >> minutes >> seconds;
+
+    result = convert(hours, minutes, seconds);
+
+    cout << "In minutes: " << result;
 
     return 0;
 }
