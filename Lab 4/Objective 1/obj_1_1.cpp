964062d53fb39d1a00442a29c0a1bab3ee011f2e
Mansij Maharjan <mansijmaharzn@gmail.com>
2023-02-01 10:40:42 +0545

    need help cant solve

--- Lab 4/Objective 1/obj_1_1.cpp
+++ Lab 4/Objective 1/obj_1_1.cpp
@@ -1,1 +1,35 @@
 // Modify obj 1 program using statis variable
+#include<iostream>
+using namespace std;
+
+// int count = 0;
+
+class contdest {
+    static int count;
+
+    public:
+        contdest() {
+            // count = 0;
+            cout << "\nObject Created = " << count++;
+        }
+
+        ~contdest() {
+            cout <<"\nObject Destroyed = " << --count;
+        }
+};
+
+int main() {
+    cout << "\nMain Block\n";
+    contdest a, b, c;
+
+    {
+        cout << "\nBlock 1\n";
+        contdest d;
+    }
+
+    cout << "\nAgain main Block\n";
+    
+    contdest e;
+
+    return 0;
+}
