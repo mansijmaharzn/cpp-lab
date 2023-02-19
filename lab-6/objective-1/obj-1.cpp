f59c8e6d97ddeeace1357521c46a8cd980fb7c7f
Mansij Maharjan <mansijmaharzn@gmail.com>
2023-02-19 14:43:43 +0545

    slved

--- lab-6/objective-1/obj-1.cpp
+++ lab-6/objective-1/obj-1.cpp
@@ -1,41 +1,41 @@
-#include<iostream.h>
-#include<conio.h>
+#include<iostream>
+using namespace std;
 
 class student {
-    private: 
     char name[10], address[20], sem[5], faculty[25];
-    int crn:
+    int crn;
     public:
-    void getdata(){
-        cout << "Enter the details of a student:\n";
-        cout << "Name: ";
-        cin >> name;
-        cout << "\nAddress: ";
-        cin >> address;
-        cout << " \nSemester: ";
-        cin >> sem;
-        cout << "\nFaculty: ";
-        cin >> faculty;
-        cout << "\nCollege roll no: ";
-        cin >> crn;
-    
-    }
-    void display (){
-        cout << "\nThe details of the students are as follows::\n";
-        cout << "\nName " << name;
-        cout << "\nAddress " << address;
-        cout << "\nSemester " << sem;
-        cout << "\nFaculty " << faculty;
-        cout << "\nCollege roll no " << crn;
+        void getdata(){
+            cout << "Enter the details of a student:\n";
+            cout << "Name: ";
+            cin >> name;
+            cout << "\nAddress: ";
+            cin >> address;
+            cout << " \nSemester: ";
+            cin >> sem;
+            cout << "\nFaculty: ";
+            cin >> faculty;
+            cout << "\nCollege roll no: ";
+            cin >> crn;
+        
+        }
+        void display (){
+            cout << "\nThe details of the students are as follows::\n";
+            cout << "\nName " << name;
+            cout << "\nAddress " << address;
+            cout << "\nSemester " << sem;
+            cout << "\nFaculty " << faculty;
+            cout << "\nCollege roll no " << crn;
 
-    }
+        }
 };
-void main(){
-    clrscr();
+
+int main(){
     student *ps;
     student s;
     ps = &s;
     ps-> getdata();
     ps-> display();
-    getch();
+
+    return 0;
 }
