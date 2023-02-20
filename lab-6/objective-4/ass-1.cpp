8b2ada86df2550b5f6fc377e57f0800eacb91f6b
ZXY-CC-3ag13 <ZXY-CC-3ag13@protonmail.com>
2023-02-20 19:15:31 +0545

    Check sir.

--- lab-6/objective-4/ass-1.cpp
+++ lab-6/objective-4/ass-1.cpp
@@ -4,15 +4,56 @@ Engineering, Science and Medical. Create their objects process them and
 access them using array of base class pointers.
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+// Base class
 class Student {
-
+public:
+    void display() {
+        cout << "Student" << endl;
+    }
 };
 
+// Derived classes
+class Engineering : public Student {
+public:
+    void display() {
+        cout << "Engineering Student" << endl;
+    }
+};
+
+class Science : public Student {
+public:
+    void display() {
+        cout << "Science Student" << endl;
+    }
+};
+
+class Medical : public Student {
+public:
+    void display() {
+        cout << "Medical Student" << endl;
+    }
+};
 
 int main() {
+    const int SIZE = 3;
+    Student* students[SIZE]; // Array of base class pointers
+
+    students[0] = new Engineering();
+    students[1] = new Science();
+    students[2] = new Medical();
+
+    // Process objects
+    for (int i = 0; i < SIZE; i++) {
+        students[i]->display();
+    }
+
+    // Free memory
+    for (int i = 0; i < SIZE; i++) {
+        delete students[i];
+    }
 
     return 0;
 }
